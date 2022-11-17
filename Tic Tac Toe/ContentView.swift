c7ce8cc49fad79cb4d7f2bd30a6c541fa65706b1
Owen Johnson <owenbjohnson10@gmail.com>
2022-11-17 09:32:38 -0600

    Added flip animation

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,22 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
     var body: some View {
+        
         VStack {
             Text("Tic Tac Toe")
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
-                Text("x")
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Color.white
+                            .opacity(moves[index] ==  "" ? 1 : 0)
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
+                                   
+                            }
+                        }
+                    }
+                    .rotation3DEffect(
+                        .init(degrees: moves[index] != "" ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0)
+                    )
+                }
+                   
+                    .preferredColorScheme(.dark)
+                    .padding()
             }
-            .preferredColorScheme(.dark)
-            .padding()
         }
     }
     
